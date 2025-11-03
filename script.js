28a1cfdfe747c5ce2a56ef58361fb0d48874adf9
komal Aulakh <kkomalpreetkaur@academic.rrc.ca>
2025-11-03 12:25:55 -0600

    added function to display fetched data in a table

--- script.js
+++ script.js
@@ -15,3 +15,34 @@ async function fetchTrees(commonName) {
         resultsDiv.innerHTML = `<p class="error">Failed to fetch data. Please try again later.</p>`;
     }
 }
+
+function displayResults(data) {
+    if (data.length === 0) {
+        resultsDiv.innerHTML = "<p>No trees found for this search.</p>";
+        return;
+    }
+
+    let tableHTML = `<table>
+                        <tr>
+                            <th>Common Name</th>
+                            <th>Scientific Name</th>
+                            <th>Diameter (cm)</th>
+                            <th>Height (m)</th>
+                            <th>Address</th>
+                        </tr>`;
+
+    data.forEach(tree => {
+        const { common_name, scientific_name, diameter_at_breast_height, height, address } = tree;
+        tableHTML += `<tr>
+                        <td>${common_name || "N/A"}</td>
+                        <td>${scientific_name || "N/A"}</td>
+                        <td>${diameter_at_breast_height || "N/A"}</td>
+                        <td>${height || "N/A"}</td>
+                        <td>${address || "N/A"}</td>
+                      </tr>`;
+    });
+
+    tableHTML += "</table>";
+    resultsDiv.innerHTML = tableHTML;
+}
+
